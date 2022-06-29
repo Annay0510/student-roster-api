80472f2d897e3f8118b3096068c71c6ace166904
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-29 03:57:25 -0400

    connected mongoose and server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-//created server js
+//import our express application
+const app = require ("./App")
+
+//import mongoose inorder to connect to our database
+const mongoose = require("mongoose");
+
+//import dotenv inorder to connect to our database
+const dotenv = require("dotenv");
+
+//use dotenv to connect to our config files
+dotenv.config({
+    path: "./config.env"
+});
+
+const students = mongoose.connect(process.env.DATABASE.replace("<password>", process.env.PASSWORD))
+.then(() => {
+    console.log("Database is running")
+})
+
+//running port
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server running on PORT ${port}`)
+})
